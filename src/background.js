ca36a1ead1a1ecaa66d7060a1ad2d39f18fb4371
Manuel Alvarez <manuel_ale94@outlook.com>
2024-05-01 19:59:38 -0600

    FIX: Icons customization for build app

--- src/background.js
+++ src/background.js
@@ -3,6 +3,7 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
+import path from 'path'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -15,6 +16,8 @@ async function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    title: "Password Generator",
+    icon: path.join(__static, 'icon.png'),
     webPreferences: {
       
       // Use pluginOptions.nodeIntegration, leave this alone
@@ -54,14 +57,14 @@ app.on('activate', () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  /* if (isDevelopment && !process.env.IS_TEST) {
+  if (isDevelopment && !process.env.IS_TEST) {
     // Install Vue Devtools
     try {
       await installExtension(VUEJS3_DEVTOOLS)
     } catch (e) {
       console.error('Vue Devtools failed to install:', e.toString())
     }
-  } */
+  }
   createWindow()
 })
 
